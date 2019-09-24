a3a2c76156eee45e40595dcdce6284fe804b6999
Mamai <mamayserg1@gmail.com>
2019-09-24 21:38:48 +0300

    Initial commitfirst commit

---
+++ components/GaleryCarousel.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <carousel :data="data" :controls="true"></carousel>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GaleryCarousel",
+        data() {
+            return {
+                data: [
+                    '<div class="example-slide">Slide 1</div>',
+                    '<div class="example-slide">Slide 2</div>',
+                    '<div class="example-slide">Slide 3</div>',
+                ],
+            };
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+  <section class="container">
+    <div>
+<GaleryCarousel></GaleryCarousel>
+    </div>
+  </section>
+    </div>
+</template>
+
+<script>
+
+import GaleryCarousel from "../components/GaleryCarousel";
+
+export default {
+  components: {
+      GaleryCarousel,
+
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
